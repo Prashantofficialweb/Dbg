6cfa63e6b08a4e44403e8f57a95e03abd7744b90
Prashantofficialweb <81702993+Prashantofficialweb@users.noreply.github.com>
2026-05-17 09:46:59 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,25 @@
+{
+  "name": "Darbhanga Sikho",
+  "short_name": "Darbhanga Sikho",
+  "description": "Darbhanga Sikho Focus on great future of Student by providing affordable price education.",
+  "start_url": "https://dbgsikho.blogspot.com/",
+  "display": "standalone",
+  "background_color": "#87f59b",
+  "theme_color": "#87f59b",
+  "orientation": "any",
+  "gcm_sender_id": "482941778795",
+  "icons": [
+    {
+      "src": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgfZeKKbcLX3oYUtgrhMJc_fr_ZIt4l2yMevHMp7swGjx8xmrY8k8KtLd9WgLdLRi4twE2yM79TPWeLpLYlS4gWi5C4C81dz8Wk3bXsBWYzGwAAOt4JEHzmt6na0PEHaY-0dD30_FBVSBPWtKst5u-9SeeOhxm_O5foXrqIXf5JScw-KQFeUNngV8jfor8/s1024/IMG_0970.webp",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgfZeKKbcLX3oYUtgrhMJc_fr_ZIt4l2yMevHMp7swGjx8xmrY8k8KtLd9WgLdLRi4twE2yM79TPWeLpLYlS4gWi5C4C81dz8Wk3bXsBWYzGwAAOt4JEHzmt6na0PEHaY-0dD30_FBVSBPWtKst5u-9SeeOhxm_O5foXrqIXf5JScw-KQFeUNngV8jfor8/s1024/IMG_0970.webp",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
